1c8c563b20d9407d102613de68d37222f114f308
Anil Thapa <anil.thapa@8squarei.com>
2024-01-28 13:32:41 +0545

    refactor: remove unused bricks

--- bricks/childview/__brick__/{{filename}}/view_model/{{filename.snakeCase()}}_viewmodel.dart
+++
@@ -1,4 +0,0 @@
-import 'package:mmp/base/base.dart';
-
-/// ViewModel used in {{filename.pascalCase()}}View
-class {{filename.pascalCase()}}ViewModel extends BaseAppViewModel {}
--- bricks/service/__brick__/lib/ui/{{modulename}}/services/{{modulename.snakeCase()}}_service.dart
+++
@@ -1,4 +0,0 @@
-import 'package:mmp/base/base.dart';
-import 'impl_{{modulename.lowerCase()}}_service.dart';
-
-class {{modulename.pascalCase()}}Service implements Impl{{modulename.pascalCase()}}Service{}
