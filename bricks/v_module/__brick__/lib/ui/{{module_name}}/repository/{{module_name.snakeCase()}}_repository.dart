05b719e6fa910667b44cda41815df434ac0b9c76
Anil Thapa <anil.thapa@8squarei.com>
2024-06-18 14:34:39 +0545

    fix: v_module respository typo

--- bricks/v_module/__brick__/lib/ui/{{module_name}}/repository/{{module_name.snakeCase()}}_repository.dart
+++ bricks/v_module/__brick__/lib/ui/{{module_name}}/repository/{{module_name.snakeCase()}}_repository.dart
@@ -1,4 +1,4 @@
 /// Repository for {{module_name}}
-class {{module_name.pascalCase()}}Service{
+class {{module_name.pascalCase()}}Repository{
   final _service = locator<{{module_name.pascalCase()}}Service>();
 }
